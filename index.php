26b4d2299f3269c02ffabf1ff98a8096db99e498
Subhankar Pal <developer.subho57@gmail.com>
2021-07-23 14:49:07 +0530

    image changed

--- index.php
+++ index.php
@@ -1945,10 +1945,10 @@
                         <!---->
                         <div _ngcontent-sc69="" class="section-landing bg-f txt-c">
                             <div _ngcontent-sc69="">
-                                <p _ngcontent-sc69="" translate="" class="news__headline h1 color-black margin-t-xl"><img src="https://razorpay.com/assets/payments/cp_modes_wallets.png" />
+                                <p _ngcontent-sc69="" translate="" class="news__headline h1 color-black margin-t-xl"><img src="https://razorpay.com/assets/upi-autopay/logo-mobile.png" />
                                     <br/>
                                     "Reinvent the pharmacy Experience"</p>
-                                <p _ngcontent-sc69="" class="news__source font-semibold small">- Vancouver Sun</p>
+                                <p _ngcontent-sc69="" class="news__source font-semibold small">Payments powered by Razorpay</p>
                             </div>
                         </div>
                         <landing-care-section _ngcontent-sc69="" _nghost-sc62="">
